0b15cbbdfd07206bc3cb5b41102c8ea9b7a5bca0
JADAV OM <jadavbaldevbhai51@gmail.com>
2026-05-11 16:32:15 +0530

    Add files via upload

---
+++ age.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main()
+{
+	int age;
+	printf("Enter your age :");
+	scanf("%d, &age");
+	
+	if(age>=18)
+	{
+	 printf("you can vote");
+	}
+	else
+	{
+	  printf("you cannot vote");
+	}
+}
